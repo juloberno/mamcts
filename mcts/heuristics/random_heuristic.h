83548b3768b55ea929c80e742109cf3be06b50e2
jbernhard <bernhard@fortiss.org>
2019-10-08 20:38:34 +0200

    random heuristic uses statistics choose next action

--- mcts/heuristics/random_heuristic.h
+++ mcts/heuristics/random_heuristic.h
@@ -22,13 +22,13 @@ public:
         //catch case where newly expanded state is terminal
         if(node->get_state()->is_terminal()){
             const AgentIdx num_agents = node->get_state()->get_agent_idx().size();
-            const ActionIdx num_actions = node->get_state()->get_num_actions(S::ego_agent_idx); 
-            SE ego_heuristic(0, S::ego_agent_idx);
+            const ActionIdx num_ego_actions = node->get_state()->get_num_actions(S::ego_agent_idx); 
+            SE ego_heuristic(num_ego_actions, S::ego_agent_idx);
             ego_heuristic.set_heuristic_estimate(0.0f, 0.0f);
             std::unordered_map<AgentIdx, SO> other_heuristic_estimates;
             for (AgentIdx ai = S::ego_agent_idx+1; ai < num_agents; ++ai)
             {   
-                SO statistic(num_actions, ai);
+                SO statistic(node->get_state()->get_num_actions(ai), ai);
                 statistic.set_heuristic_estimate(0.0f, 0.0f);
                 other_heuristic_estimates.insert(std::pair<AgentIdx, SO>(ai, statistic));
             }
@@ -39,7 +39,6 @@ public:
         auto start = std::chrono::high_resolution_clock::now();
         std::shared_ptr<S> state = node->get_state()->clone();
         const AgentIdx num_agents = node->get_state()->get_agent_idx().size();
-        const ActionIdx num_actions = node->get_state()->get_num_actions(S::ego_agent_idx);
 
         std::vector<Reward> accum_rewards(num_agents);
         std::vector<Reward> step_rewards(state->get_agent_idx().size());
@@ -48,9 +47,20 @@ public:
         const double k_discount_factor = mcts::MctsParameters::DISCOUNT_FACTOR; 
         double modified_discount_factor = k_discount_factor;
         int num_iterations = 0;
+        
         while((!state->is_terminal())&&(num_iterations<max_iterations_random_heuristic)&&(std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now() - start ).count() < max_search_time_random_heuristic ))
         {
-             auto new_state = state->execute(random_joint_action(num_actions, num_agents), step_rewards, ego_cost);
+             // Build joint action by calling statistics for each agent
+             JointAction jointaction(num_agents);
+             SE ego_statistic(node->get_state()->get_num_actions(S::ego_agent_idx), S::ego_agent_idx);
+             jointaction[S::ego_agent_idx] = ego_statistic.choose_next_action(*state);
+             for (AgentIdx ai = S::ego_agent_idx+1; ai < num_agents; ++ai)
+             {   
+                SO statistic(node->get_state()->get_num_actions(ai), ai);
+                jointaction[ai] = statistic.choose_next_action(*state);
+             }
+
+             auto new_state = state->execute(jointaction, step_rewards, ego_cost);
              state = new_state->clone();
              num_iterations +=1;
              // discount the rewards of the current step
@@ -68,25 +78,14 @@ public:
         std::unordered_map<AgentIdx, SO> other_heuristic_estimates;
         for (AgentIdx ai = S::ego_agent_idx+1; ai < num_agents; ++ai)
         {   
-            SO statistic(num_actions, ai);
+            SO statistic(0, ai);
             statistic.set_heuristic_estimate(accum_rewards[ai], accum_cost);
             other_heuristic_estimates.insert(std::pair<AgentIdx, SO>(ai, statistic));
         }
         return std::pair<SE, std::unordered_map<AgentIdx, SO>>(ego_heuristic, other_heuristic_estimates);
     }
 
-    JointAction random_joint_action(ActionIdx num_actions, AgentIdx num_agents)
-    {
-        std::uniform_int_distribution<ActionIdx> random_action_selection(0,num_actions-1);
-
-        auto gen = [&](){
-            return random_action_selection(random_generator_);
-        };
-
-        JointAction ja(num_agents);
-        std::generate(std::begin(ja), std::end(ja), gen);
-        return ja;
-    }
+private:
     const double max_search_time_random_heuristic = mcts::MctsParameters::RandomHeuristic::MAX_SEARCH_TIME;
     const double max_iterations_random_heuristic = mcts::MctsParameters::RandomHeuristic::MAX_NUMBER_OF_ITERATIONS;
 
