2105e17a7274b7a7b7af05b0183a8dbeebe77624
juloberno <juloberno@gmail.com>
2020-08-04 13:14:30 +0200

    cost constrained test adapt cost based on probability

--- test/cost_constrained/cost_constrained_test.cc
+++ test/cost_constrained/cost_constrained_test.cc
@@ -47,7 +47,7 @@ struct CostConstrainedTest : public ::testing::Test {
             mcts_parameters_.cost_constrained_statistic.REWARD_UPPER_BOUND = std::max(goal_reward1_, goal_reward2_);
             mcts_parameters_.cost_constrained_statistic.REWARD_LOWER_BOUND = 0.0f;
             mcts_parameters_.cost_constrained_statistic.COST_LOWER_BOUND = 0.0f;
-            mcts_parameters_.cost_constrained_statistic.COST_UPPER_BOUND = 1.0f;
+            mcts_parameters_.cost_constrained_statistic.COST_UPPER_BOUND = 1.0f/std::min(risk_action1, risk_action2);
             mcts_parameters_.cost_constrained_statistic.KAPPA = 10.0f;
             mcts_parameters_.cost_constrained_statistic.GRADIENT_UPDATE_STEP = 1.0f;
             mcts_parameters_.cost_constrained_statistic.TAU_GRADIENT_CLIP = 1.0f;
@@ -103,8 +103,8 @@ TEST_F(CostConstrainedTest, one_step_higher_reward_higher_risk_constraint_eq) {
   const auto& cost_stats = root.get_ego_int_node().get_cost_ucb_statistics();
 
   // Cost statistics desired
-  EXPECT_NEAR(cost_stats.at(2).action_value_, risk_action2_, 0.08);
-  EXPECT_NEAR(cost_stats.at(1).action_value_, risk_action1_, 0.08);
+  EXPECT_NEAR(cost_stats.at(2).action_value_, 1.0, 0.08);
+  EXPECT_NEAR(cost_stats.at(1).action_value_, 1.0, 0.08);
   EXPECT_NEAR(cost_stats.at(0).action_value_, 0, 0.00);
 
   // Reward statistics desired
@@ -126,8 +126,8 @@ TEST_F(CostConstrainedTest, one_step_higher_reward_higher_risk_constraint_lower)
   const auto& cost_stats = root.get_ego_int_node().get_cost_ucb_statistics();
 
   // Cost statistics desired
-  EXPECT_NEAR(cost_stats.at(2).action_value_, risk_action2_, 0.05);
-  EXPECT_NEAR(cost_stats.at(1).action_value_, risk_action1_, 0.05);
+  EXPECT_NEAR(cost_stats.at(2).action_value_, 1.0, 0.05);
+  EXPECT_NEAR(cost_stats.at(1).action_value_, 1.0, 0.05);
   EXPECT_NEAR(cost_stats.at(0).action_value_, 0, 0.00);
 
   // Reward statistics desired
@@ -149,8 +149,8 @@ TEST_F(CostConstrainedTest, one_step_higher_reward_eq_risk_constraint_higher) {
   const auto& cost_stats = root.get_ego_int_node().get_cost_ucb_statistics();
 
   // Cost statistics desired
-  EXPECT_NEAR(cost_stats.at(2).action_value_, risk_action2_, 0.05);
-  EXPECT_NEAR(cost_stats.at(1).action_value_, risk_action1_, 0.05);
+  EXPECT_NEAR(cost_stats.at(2).action_value_, 1.0, 0.08);
+  EXPECT_NEAR(cost_stats.at(1).action_value_, 1.0, 0.08);
   EXPECT_NEAR(cost_stats.at(0).action_value_, 0, 0.00);
 
   // Reward statistics desired
@@ -164,7 +164,7 @@ TEST_F(CostConstrainedTest, one_step_higher_reward_eq_risk_constraint_higher) {
 }
 
 TEST_F(CostConstrainedNStepTest, n_step_higher_reward_higher_risk_constraint_eq) {
-  SetUp(3, 2.0f, 0.5f, 0.8f, 0.4f, 0.5f, 2.0f, true, 8000);
+  SetUp(3, 2.0f, 0.5f, 0.8f, 0.4f, 0.42f, 2.0f, true, 8000);
 
   int num_samples = 20;
   int num_collisions = 0;
@@ -203,7 +203,7 @@ TEST_F(CostConstrainedNStepTest, n_step_higher_reward_higher_risk_constraint_eq)
   const double collision_risk = double(num_collisions)/num_samples;
   const double goal_rate = double(num_goal_reached)/num_samples;
   LOG(INFO) << "Collision risk:" << collision_risk;
-  EXPECT_TRUE(collision_risk <= 0.5);
+  EXPECT_TRUE(collision_risk <= 0.42);
 }
 
 
