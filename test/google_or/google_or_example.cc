ffd2c357866503d1d81323f93ab062b7d7a5194e
juloberno <juloberno@gmail.com>
2020-11-04 11:11:56 +0100

    example working

--- test/google_or/google_or_example.cc
+++ test/google_or/google_or_example.cc
@@ -3,8 +3,9 @@
 #include <unordered_map>
 
 namespace operations_research {
+
 void SimpleLpProgram() {
-    std::vector<double> action_values{0.05, 0.1, 0.2, 0.3};
+    std::vector<std::vector<double>> action_values{{0.6, 0.1, 0.2, 0.3}, {0.1, 0.7, 0.01, 0.3}};
     std::vector<double> lambdas{0.2, 0.3};
     std::vector<double> constraints{0.3, 0.0};
 
@@ -18,7 +19,7 @@ void SimpleLpProgram() {
     std::vector<MPVariable* > pos_errors;
     std::vector<MPVariable* > neg_errors;
 
-    for (std::size_t action_idx = 0; action_idx <  action_values.size(); ++action_idx) {
+    for (std::size_t action_idx = 0; action_idx <  action_values[0].size(); ++action_idx) {
         action_weights.push_back(solver.MakeNumVar(0.0, 1.0, std::string("w_") + std::to_string(action_idx)));
     }
 
@@ -33,8 +34,8 @@ void SimpleLpProgram() {
     for (std::size_t lambda_idx = 0; lambda_idx <  lambdas.size(); ++lambda_idx) {
         MPConstraint* const ct = solver.MakeRowConstraint(constraints[lambda_idx], constraints[lambda_idx],
                                                             std::string("c") + std::to_string(lambda_idx));
-        for (std::size_t action_idx = 0; action_idx <  action_values.size(); ++action_idx) {
-            ct->SetCoefficient(action_weights[action_idx], action_values[action_idx]);
+        for (std::size_t action_idx = 0; action_idx <  action_values[lambda_idx].size(); ++action_idx) {
+            ct->SetCoefficient(action_weights[action_idx], action_values[lambda_idx][action_idx]);
         }
         for (std::size_t error_idx = 0; error_idx <  lambdas.size(); ++error_idx) {
             if (error_idx == lambda_idx) {
@@ -49,7 +50,7 @@ void SimpleLpProgram() {
 
     // constraints for weight sum
     MPConstraint* const ct_sum = solver.MakeRowConstraint(1.0, 1.0, "weight_sum");
-    for (std::size_t action_idx = 0; action_idx <  action_values.size(); ++action_idx) {
+    for (std::size_t action_idx = 0; action_idx <  action_values[0].size(); ++action_idx) {
             ct_sum->SetCoefficient(action_weights[action_idx], 1.0);
     }
     for (std::size_t error_idx = 0; error_idx <  lambdas.size(); ++error_idx) {
@@ -61,7 +62,7 @@ void SimpleLpProgram() {
 
     // Create the objective function, 3 * x + y.
     MPObjective* const objective = solver.MutableObjective();
-    for (std::size_t action_idx = 0; action_idx <  action_values.size(); ++action_idx) {
+    for (std::size_t action_idx = 0; action_idx <  action_values[0].size(); ++action_idx) {
             objective->SetCoefficient(action_weights[action_idx], 0.0);
     }
     for (std::size_t error_idx = 0; error_idx <  lambdas.size(); ++error_idx) {
@@ -73,11 +74,18 @@ void SimpleLpProgram() {
     auto start = std::chrono::high_resolution_clock::now();
     solver.Solve();
     unsigned int duration =
-      std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now() - start ).count();
+      std::chrono::duration_cast<std::chrono::microseconds>( std::chrono::high_resolution_clock::now() - start ).count();
 
     LOG(INFO) << "Solution:" << std::endl;
     LOG(INFO) << "Objective value = " << objective->Value();
     LOG(INFO) << "Runtime: " << duration; 
+    for (std::size_t action_idx = 0; action_idx <  action_values[0].size(); ++action_idx) {
+         LOG(INFO) << "Solution a" << action_idx << " = "  << action_weights[action_idx]->solution_value(); 
+    }
+    for (std::size_t error_idx = 0; error_idx <  lambdas.size(); ++error_idx) {
+        LOG(INFO) << "Solution ne" << error_idx << " = "  << neg_errors[error_idx]->solution_value(); 
+        LOG(INFO) << "Solution pe" << error_idx << " = "  << pos_errors[error_idx]->solution_value(); 
+    }
     }
     }  // namespace operations_research
 
